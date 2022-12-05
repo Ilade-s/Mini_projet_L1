adcd3abf13e53064dcf76ea9551ceb63369ca0cd
Raphaël Merlet <76600778+Ilade-s@users.noreply.github.com>
2022-12-05 16:40:05 +0000

    affichages et lecture couts et prods

--- projet.cpp
+++ projet.cpp
@@ -6,6 +6,7 @@ MERLET Raphaël
 #include <iostream>
 #include <fstream>
 #include <cassert>
+#include <string>
 #include "liste.hpp"
 using namespace std;
 
@@ -44,7 +45,7 @@ struct Couts
     float thermique, nucleaire, eolien, solaire, hydro, bio;
 };
 
-// lectures et afichages
+// lectures et affichages
 
 void afficher_horodatage(Horodatage temps){
     cout << temps.mois << " " << temps.jour << " " << temps.heure;
@@ -62,6 +63,27 @@ void afficher_tache(Tache task){
         cout << task.regions[i] << " ";
     }
     cout << endl;
+}
+
+Couts lire_couts(string filename){
+    Couts cost;
+    fstream flux;
+    flux.open(filename, ios::in);
+    if (flux.is_open())
+    {
+        flux >> cost.thermique;
+        flux >> cost.nucleaire;
+        flux >> cost.eolien;
+        flux >> cost.solaire;
+        flux >> cost.hydro;
+        flux >> cost.bio;
+    }
+    else
+    {
+        cout << "Erreur : impossible d’ouvrir "  << filename << endl;
+    }
+
+    return cost;
 }
 
 Tache lire_fichier_tache(string filename){
@@ -95,6 +117,38 @@ Tache lire_fichier_tache(string filename){
     return task;
 }
 
+liste<Production> lire_donnees(string filename, int limit){
+    Production prod;
+    liste<Production> prods;
+    fstream flux;
+    int i = 0;
+    flux.open(filename, ios::in);
+    if (flux.is_open())
+    {
+        while (flux.good() and i < limit) {
+            flux >> prod.region;
+            flux >> prod.temps.mois;
+            flux >> prod.temps.jour;
+            flux >> prod.temps.heure;
+            flux >> prod.thermique;
+            flux >> prod.nucleaire;
+            flux >> prod.eolien;
+            flux >> prod.solaire;
+            flux >> prod.hydro;
+            flux >> prod.bio;
+            flux >> prod.solde;
+            inserer(prod, prods, taille(prods) + 1);
+            i++;
+        }
+    }
+    else
+    {
+        cout << "Erreur : impossible d’ouvrir "  << filename << endl;
+    }
+
+    return prods;
+}
+
 void afficher_couts(Couts c_ener){
     cout << c_ener.thermique << " " << c_ener.nucleaire << " "
          << c_ener.eolien << " " << c_ener.solaire << " "
@@ -110,8 +164,19 @@ void afficher_production(Production prod){
 
 }
 
+void afficher_donnees(liste<Production> l_prod){
+    for (Production prod : l_prod) {
+        afficher_production(prod);
+        cout << endl;
+    }
+}
+
 int main(){
     Tache task = lire_fichier_tache("tache1.txt");
     afficher_tache(task);
+    Couts cost = lire_couts("couts.txt");
+    afficher_couts(cost);
+    liste<Production> prods = lire_donnees("energieFrance2021.txt", 10);
+    afficher_donnees(prods);
     return 0;
 }
