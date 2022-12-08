a9fd9ac292579ab73fd34c9a8bccd1d2ad9d0d80
Ilade-s <76600778+Ilade-s@users.noreply.github.com>
2022-12-08 01:17:31 +0100

    description projet.cpp

--- projet.cpp
+++ projet.cpp
@@ -1,6 +1,9 @@
 /*
-Mini-projet fin S1L1
-MERLET Raphaël
+Mini-projet fin S1L1, Base en info
+MERLET Raphaël, Bilal RAHALI
+
+Permet la planification d'une tâche décrite dans un fichier, avec un fichier de coûts et de productions fournis
+Les solutions trouvées sont enregistrées dans un fichier de sortie (au nom indiqué dans le fichier de tâche)
 */
 
 #include <iostream>
